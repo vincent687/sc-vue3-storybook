658d7e18e17693ddf06bbadbc4b9acc38b0215f0
vincent wong <vincentwong687@gmail.com>
2021-12-05 21:31:49 +0800

    add card component and first setup

---
+++ src/stories/README.md
@@ -0,0 +1,6 @@
+# Vue 3 + Typescript +Storybook
+
+# PostCSS plugin autoprefixer requires PostCSS 8.
+
+https://tailwindcss.com/docs/installation#post-css-7-compatibility-build
+@tailwindcss/postcss7-compat
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        gray: colors.trueGray,
+        indigo: colors.indigo,
+        red: colors.rose,
+        yellow: colors.amber,
+        peach: "#fa9470",
+        green: colors.green,
+      },
+      gridTemplateColumns: {
+        "job-slides": "repeat(1, 200px)",
+        "job-slides--md": "repeat(1, 300px)",
+      },
+      gridAutoColumns: {
+        "job-slides": "200px",
+        "job-slides--md": "300px",
+      },
+      gridTemplateRows: {
+        "job-slides": "minmax(250px, 1fr)",
+        "job-slides--md": "minmax(300px, 1fr)",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
